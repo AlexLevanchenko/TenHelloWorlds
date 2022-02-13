8ac5d5fdd7845e3c840124c6ce1422c8fa527fb4
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-02-13 14:23:46 +0200

    Initial commit

---
+++ src/TenHelloWorlds.java
@@ -0,0 +1,12 @@
+/**
+ * A program that prints <code>Hello itHillel</code> 10 times
+ *
+ * @author Alex Levanchenko
+ */
+public class TenHelloWorlds {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello itHillel :)");
+        }
+    }
+}
